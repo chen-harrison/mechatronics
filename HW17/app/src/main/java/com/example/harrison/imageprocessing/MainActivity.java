f530762acaa077a83c2ebea7fa42912adbc345cf
Harrison <harrisonchen2019@u.northwestern.edu>
2018-06-17 11:59:00 -0500

    hw17

--- HW17/app/src/main/java/com/example/harrison/imageprocessing/MainActivity.java
+++ HW17/app/src/main/java/com/example/harrison/imageprocessing/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.harrison.androidcamera;
+package com.example.harrison.imageprocessing;
 
 // libraries
 
@@ -37,11 +37,34 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
     private Canvas canvas = new Canvas(bmp);
     private Paint paint1 = new Paint();
     private TextView mTextView;
+    SeekBar myControl;
+    public int thresh = 0;
 
-    public int sum = 0;
-    public int counter = 0;
 
     static long prevtime = 0; // for FPS calculation
+
+    private void setMyControlListener() {
+        myControl.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+            int progressChanged = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+                thresh = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+        });
+    }
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -101,28 +124,33 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
     public void onSurfaceTextureUpdated(SurfaceTexture surface) {
         // every time there is a new Camera preview frame
         mTextureView.getBitmap(bmp);
+        myControl = (SeekBar) findViewById(R.id.seek1);
+
+        setMyControlListener();
 
         final Canvas c = mSurfaceHolder.lockCanvas();
         if (c != null) {
-            int thresh = 0; // comparison value
-            int[] pixels = new int[bmp.getWidth()]; // pixels[] is the RGBA data
-            int startY = 240; // which row in the bitmap to analyze to read
-            bmp.getPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
+            for (int j = 0; j < bmp.getHeight()/4; j++){
+                // int thresh = 0; // comparison value
+                int[] pixels = new int[bmp.getWidth()]; // pixels[] is the RGBA data
+                int startY = 4*j; // which row in the bitmap to analyze to read
+                bmp.getPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
 
-            // in the row, see if there is more green than red
-            for (int i = 0; i < bmp.getWidth(); i++) {
-                if ((red(pixels[i] - green(pixels[i]))) > thresh) {
-                    sum = sum + i;
-                    counter++;
+                // in the row, see if there is more green than red
+                for (int i = 0; i < bmp.getWidth(); i++) {
+                    if ((green(pixels[i]) - red(pixels[i])) > thresh) {
+                        pixels[i] = rgb(0, 255, 0); // over write the pixel with pure green
+                    }
                 }
+
+                // update the row
+                bmp.setPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
             }
 
-            // update the row
-            // bmp.setPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
         }
 
-        // draw a circle at the average position at which the red pixels were found at y = 240
-        int pos = sum / counter;
+        // draw a circle at some position
+        int pos = 50;
         canvas.drawCircle(pos, 240, 5, paint1); // x position, y position, diameter, color
 
         // write the pos as text
@@ -137,3 +165,4 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
         prevtime = nowtime;
     }
 }
+
